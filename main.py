8e742e7c089212898093cc064055684af88c99fa
MitakaBaby <122027869+MitakaBaby@users.noreply.github.com>
2024-04-22 14:02:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from scheduling import sites_to_run
 from scrapemethods import Methods
 
 if __name__ == "__main__":
-    sites = sites_to_run()
-    for site in sites:
-        config = Utils.load_configs(site)
-        method_name = config.get("scrape_method")
-        if method_name:
-            site_processor = Methods()
-            if hasattr(site_processor, method_name):
-                try:
-                    method_to_call = getattr(site_processor, method_name)
-                    method_to_call(site)
-                except Exception as e:
-                    print(e)
-                    continue
+    while True:
+        sites = sites_to_run()
+        for site in sites:
+            config = Utils.load_configs(site)
+            method_name = config.get("scrape_method")
+            if method_name:
+                site_processor = Methods()
+                if hasattr(site_processor, method_name):
+                    try:
+                        method_to_call = getattr(site_processor, method_name)
+                        method_to_call(site)
+                    except Exception as e:
+                        print(e)
+                        continue
